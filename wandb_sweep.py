c88023b3d8273141b81fb6bfcf311f2eda58540f
root <joshua@paperspace.com>
2022-03-25 04:52:13 +0000

    Update

--- wandb_sweep.py
+++ wandb_sweep.py
@@ -15,9 +15,10 @@ def main(argv):
         if opt in ('-c', '--config'):
             config_name = arg
 
-    with open('/inputs/configs/data/' + config_name + '.json') as json_file:
+    with open('/inputs/configs/' + config_name + '.json') as json_file:
         sweep_config = json.load(json_file)
 
+    wandb.login(key='1305ff3ca47ed8cd6735ba50a3b2f6697ff94916')
     sweep_id = wandb.sweep(sweep_config, project="prallel-project")
     wandb.agent(sweep_id, function=train, count=4)
 
